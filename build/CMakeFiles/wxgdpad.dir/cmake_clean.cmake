be7457df9446064afb9cb1c31054a1eb7ce92f78
Relative <kezin636@gmail.com>
2024-11-01 11:51:52 +0300

    perfect

---
+++ build/CMakeFiles/wxgdpad.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wxgdpad.dir/wxgdpad.cpp.o"
+  "CMakeFiles/wxgdpad.dir/wxgdpad.cpp.o.d"
+  "wxgdpad"
+  "wxgdpad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wxgdpad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
